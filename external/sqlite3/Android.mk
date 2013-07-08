e243666bbf0f56646375a36a1ffb835d1670f418
Anja Ziplinsky <anja.ziplinsky@scoyo.com>
2013-07-08 11:39:17 +0200

    include sqlite3 src

--- external/sqlite3/Android.mk
+++ external/sqlite3/Android.mk
@@ -1,17 +1,29 @@
 LOCAL_PATH := $(call my-dir)
 
+######################################
+# Script welches fast alle 'LOCAL_*' Variablen löscht.
+######################################
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libsqlite_static
+######################################
+# Eindeutiger Name des Moduls. Darf keine Leerzeichen enthalten
+######################################
+LOCAL_MODULE := sqlite3_static
 
+######################################
+# Eindeutiger Name des Moduls
+# Default: lib$(LOCAL_MODULE).so
+######################################
 LOCAL_MODULE_FILENAME := libsqlite3
 
-LOCAL_SRC_FILES := \
-CCConfiguration.cpp
+######################################
+# Alle Source-Dateien, getrennt durch ein Leerzeichen
+# Pfade sind relativ zu LOCAL_PATH
+######################################
+LOCAL_SRC_FILES := src/sqlite3.c
 
+######################################
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-
 
 include $(BUILD_STATIC_LIBRARY)
